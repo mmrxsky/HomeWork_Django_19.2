f425c22b44721c77ee23a49d757d32de8076ca9d
mmrxsky <mmrxsky@gmail.com>
2024-08-19 20:06:59 +0300

    Добавил urls к шаблонам и настройки DEFAULT_AUTO_FIELD setting.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,5 +5,6 @@ from catalog.views import home, contacts
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', )
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts'),
 ]
